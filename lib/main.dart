eda90a4f15776f13db97badfabc5baa879b3286d
Poppy Sari <poppy.sari@rga.com>
2021-11-07 23:10:01 +0700

    :sparkles: add basic question answer

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,52 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> answers = [false, true, true];
+
+  int questionNumber = 0;
+
+  bool hasQuestion() {
+    int nextQuestion = questionNumber + 1;
+    int questionSize = questions.length;
+    return nextQuestion < questionSize;
+  }
+
+  void nextQuestion() {
+    setState(() {
+      questionNumber++;
+    });
+  }
+
+  void checkAnswer(bool answer) {
+    bool lastQuestionAnswered = scoreKeeper.length == questions.length;
+    if (lastQuestionAnswered) {
+      return;
+    }
+
+    bool isCorrect = answer == answers[questionNumber];
+    setState(() {
+      if (isCorrect) {
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +83,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +107,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                checkAnswer(true);
+                if (hasQuestion()) {
+                  nextQuestion();
+                }
               },
             ),
           ),
@@ -80,18 +129,24 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                checkAnswer(false);
+                if (hasQuestion()) {
+                  nextQuestion();
+                }
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
 }
 
 /*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
+'You can lead a cow down stairs but not up stairs.', false,
+'Approximately one quarter of human bones are in the feet.', true,
+'A slug\'s blood is green.', true,
 */
